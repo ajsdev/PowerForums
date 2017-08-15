8399352a3bca41a7ce0bf4c8f4485a64539917dd
ᦖєgιт <support@legitsoulja.info>
2017-08-14 21:17:03 -0400

    nice autoloader for ya

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,1 +1,19 @@
+<?php session_start();
+$enum = [
+  "root"=>$_SERVER["DOCUMENT_ROOT"],
+  "lib"=>$_SERVER["DOCUMENT_ROOT"]."/lib"
+];
 
+foreach($enum as $k => $v) define($k, $v);
+
+$load = function($dir = lib."/*") use (&$load) {
+  foreach(glob($dir) as $content){
+    if(is_dir($content)) {
+      $load($content."/*");
+      continue;
+    }
+    if(strtolower(pathinfo($content)["extension"]) == "php") 
+      include $content;
+  }
+}
+$load();
